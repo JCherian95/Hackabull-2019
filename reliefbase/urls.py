a2fe61c02919ad6fbb83626d2ea6f94763846c7c
Jestin Cherian <cherian.jestin@gmail.com>
2019-03-23 16:54:33 -0400

    Updated the Django

--- reliefbase/urls.py
+++ reliefbase/urls.py
@@ -1,4 +1,4 @@
-"""DisasterRelief URL Configuration
+"""reliefbase URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -18,5 +18,4 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
 ]
--- reliefbase/views.py
+++ reliefbase/views.py
@@ -21,26 +21,3 @@ def index(request):
             print("Goodbye")
 
     return render(request, 'index.html')
-
-#dashboard Logic
-def dashboard(request):
-    if request.user.is_authenticated:
-        return render(request, 'dashboard/dashboard.html')
-
-def profile(request):
-    if not request.user.is_authenticated:
-        return redirect('login')
-
-    if request.method == 'POST':
-        form = UserProfileChangeForm(request.POST, instance=request.user)
-        print(form.errors)
-        if form.is_valid():
-            for field in form.cleaned_data:
-                if form.cleaned_data[field] != getattr(request.user, field):
-                    setattr(request.user, field, form.cleaned_data[field])
-                    request.user.save()
-                else:
-                    form = UserProfileChangeForm(instance=request.user)
-    else:
-        form = UserProfileChangeForm(instance=request.user)
-    return render(request, 'profile.html', {'form': form})
