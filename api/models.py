4306a66fc71fa7d522c822f56c4f0d137f2d379a
Jestin Cherian <cherian.jestin@gmail.com>
2019-03-23 22:40:45 -0400

    Updating Models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=128, null=False, blank=False, unique=True)
+    quantity = models.IntegerField(max_length=None, blank=False)
--- backend/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Item(models.Model):
-    id = models.IntegerField(unique=True)
-    name = models.CharField(max_length=128, null=False, blank=False, unique=True)
-    quantity = models.IntegerField(max_length=None, blank=False)
