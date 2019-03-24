6b232cde4404af2a11d3a00f2fba140a52d3b4a1
Jestin Cherian <cherian.jestin@gmail.com>
2019-03-23 20:23:50 -0400

    Added a little flare

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
       return (
       <div>
-        <button type="button" onClick={this.onClick}>Send GET</button>
+        <button type="button" onClick={this.onClick} class="btn btn-primary">Send GET</button>
       </div>
       );
   }
